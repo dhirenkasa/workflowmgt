e170cfe8007e49b18c884adb7981078c0d946c90
Dhiren Arvindbhai Kasa <dhirenk@cybage.com>
2019-09-26 18:54:21 +0530

    Footer template issue resolved

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  templateUrl: './footer.component.html',
 })
+
 export class FooterComponent {
 }
